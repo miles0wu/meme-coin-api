cb5df34ef73773b60c51d2f58c494e5b2d8aa662
Miles Wu <miles4w701@gmail.com>
2025-02-19 16:35:24 +0800

    feat: implement graceful shutdown

--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 	"go.uber.org/zap"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
+// App
 // @title MemeCoins
 // @version 0.1.0
 // @description Meme coins api swagger
@@ -26,7 +34,33 @@ func main() {
 	initViper()
 	initLogger()
 	app := InitApp()
-	app.server.Run(":8080")
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: app.server,
+	}
+
+	// start server
+	go func() {
+		zap.L().Info("Server starting", zap.String("addr", srv.Addr))
+		if err := srv.ListenAndServe(); err != nil && errors.Is(err, http.ErrServerClosed) {
+			zap.L().Fatal("listen: ", zap.Error(err))
+		}
+	}()
+
+	// listening signal for graceful shutdown
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	zap.L().Info("Shutting down server...")
+
+	// shutdown timeout 5 secs
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		zap.L().Fatal("Server forced to shutdown", zap.Error(err))
+	}
+
+	zap.L().Info("Server exiting")
 }
 
 func initViper() {
